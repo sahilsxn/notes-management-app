7926a4b7728673a971ff7cea9868c27406843b87
saxenasahil <67691585+sahilsxn@users.noreply.github.com>
2022-03-22 18:40:00 +0530

    chore: created build

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -22,7 +22,7 @@ const Home = (props) => {
                     <h2 style={{paddingBottom:'16px', textAlign: 'center'}}>Login or Register to Add/Manage Notes</h2>
                     </div>
                     <div style={{textAlign:'center', marginTop:'32px', background:'#F5F5F5', padding:'16px', borderRadius:'12px'}}>
-                    <h4>Login Credentials</h4>
+                    <h4>Credentials</h4>
                     <p><b>Email</b>: dct6543@gmail.com </p>
                     <button style={{marginBottom:'24px'}} class="btn btn-outline-primary" value="dct6543@gmail.com" onClick={handleCopy}>Copy</button>
                     <p><b>Password</b>: secret123</p>
